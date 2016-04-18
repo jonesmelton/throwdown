c213da3328bb2c6910bc853d72125b4ee16c4516
Mitch Kroska & Jones Melton <pair+kromitj.shavah@devbootcamp.com>
2016-04-18 17:02:45 -0500

    Add login and logout features:

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -8,8 +8,10 @@ class SessionController < ApplicationController
   def login
     @user = User.find_by(username: params[:session][:username])
     if @user.authenticate(params[:session][:password])
-      login(@user)
-      redirect_to leagues_path
+      log_in(@user)
+      puts "user id:--------------------------------------"
+      puts session[:user_id]
+      redirect_to user_show_path
     else
       flash.now[:danger] = 'Incorrect username/password input'
       render 'new'
