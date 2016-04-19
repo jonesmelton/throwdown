d3024e580936c708e787ffe839566d286d56688d
Jones Melton & Eric Tenza <pair+shavah.tenzaej@devbootcamp.com>
2016-04-19 17:39:45 -0500

    Add team creation

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -5,4 +5,23 @@ class TeamsController < ApplicationController
 
   def index
   end
+
+  def new
+    @team = Team.new
+    render 'new'
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to team_path(@team)
+    else
+      render 'new'
+    end
+  end
+
+   private
+  def team_params
+    params.require(:team).permit(:name, :league)
+  end
 end
