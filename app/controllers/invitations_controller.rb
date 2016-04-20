1598d4e8d3c777a9921c299823e6c8c8a3d5caac
Connor Reaumond <creaumond@gmail.com>
2016-04-19 22:16:06 -0500

    Finish most of invitations

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -6,7 +6,7 @@ class InvitationsController < ApplicationController
 
   def update
     @invitation = Invitation.find(params[:id])
-    if @invitation.update_attributes(invite_params)
+    if @invitation.update_attributes(accepted_params)
       redirect_to show
     else
       render 'show'
@@ -18,12 +18,23 @@ class InvitationsController < ApplicationController
   end
 
   def create
+    @invitation.new(invite_params)
+    @team = Team.find(params[:team_id])
+    @invitation.team = @team
+
+    if invitation.save
+      redirect_to @team
+    else
+      render 'new'
+    end
     
   end
 
   private
-
   def invite_params
+    params.require(:invitation).permit(:user_id, :team_id)
+  end
+  def accepted_params
     params.require(:invitation).permit(:accepted)
   end
 end
